f6096b878ffdfb822fd4bdf413c0da9e051ef815
Zaixu Cui <ZaixuCui@users.noreply.github.com>
2017-10-23 11:52:17 -0400

    Update PatternLocalization_MainFunction.m

--- Pattern_Localization/PatternLocalization_MainFunction.m
+++ Pattern_Localization/PatternLocalization_MainFunction.m
@@ -1,6 +1,22 @@
 
 function PatternLocalization_MainFunction(Origin_Accuracy_Path, Rand_Accuracy_Path_Cell, Origin_w_Brain, Rand_w_Brain_Cell)
 
+% Origin_Accuracy_Path:
+%     The path of a .mat file, which contains a variable named 'Accuracy' representing the prediction accuracy.
+%
+% Rand_Accuracy_Path_Cell:
+%     n*1 cell
+%     Each element is a path of .mat file, which contains a variable named 'Accuracy' representing of the prediction accuracy of random sample (permutation test)
+%
+% Origin_w_Brain:
+%     path of .mat file
+%     a variable named 'w_Brain' should be stored in the mat file
+%
+% Rand_w_Brain_Cell:
+%     cell of paths of .mat files
+%     a variable named 'w_Brain' should be stored in each mat file
+%
+
 [ResultantFolder y z] = fileparts(Origin_Accuracy_Path);
 
 disp('display accuracy distribution in random state:');
