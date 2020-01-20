d751b7469ed5d7819e26967a8f8f2670b88f5628
Zaixu Cui <zaixucui@ZaixudeMacBook-Pro.local>
2020-01-20 23:26:54 +0800

    codes

--- SVC/SVM_2group_NFolds.m
+++ SVC/SVM_2group_NFolds.m
@@ -18,7 +18,7 @@ function [Accuracy, Sensitivity, Specificity] = SVM_2group_NFolds(Subjects_Data,
 %           the path of folder storing resultant files
 %
 
-if ~exist(ResultantFolder, 'dir')
+if nargin > 4 & ~exist(ResultantFolder, 'dir')
     mkdir(ResultantFolder);
 end
 
@@ -106,25 +106,27 @@ Y_group1 = decision_values(Group1_Index);
 Category_group0 = predicted_labels(Group0_Index);
 Y_group0 = decision_values(Group0_Index);
 
-save([ResultantFolder filesep 'Y.mat'], 'Y_group1', 'Y_group0');
-save([ResultantFolder filesep 'Category.mat'], 'Category_group1', 'Category_group0');
-
 group0_Wrong_ID = find(Category_group0 == 1);
 group0_Wrong_Quantity = length(group0_Wrong_ID);
 group1_Wrong_ID = find(Category_group1 == -1);
 group1_Wrong_Quantity = length(group1_Wrong_ID);
 disp(['group0: ' num2str(group0_Wrong_Quantity) ' subjects are wrong ' mat2str(group0_Wrong_ID) ]);
 disp(['group1: ' num2str(group1_Wrong_Quantity) ' subjects are wrong ' mat2str(group1_Wrong_ID) ]);
-save([ResultantFolder filesep 'WrongInfo.mat'], 'group0_Wrong_Quantity', 'group0_Wrong_ID', 'group1_Wrong_Quantity', 'group1_Wrong_ID');
 Accuracy = (Subjects_Quantity - group0_Wrong_Quantity - group1_Wrong_Quantity) / Subjects_Quantity;
 disp(['Accuracy is ' num2str(Accuracy) ' !']);
-save([ResultantFolder filesep 'Accuracy.mat'], 'Accuracy');
 Sensitivity = (length(Group0_Index) - group0_Wrong_Quantity) / length(Group0_Index);
 Specificity = (length(Group1_Index) - group1_Wrong_Quantity) / length(Group1_Index);
 disp(['Sensitivity is ' num2str(Sensitivity) ' !']);
-save([ResultantFolder filesep 'Sensitivity.mat'], 'Sensitivity');
 disp(['Specificity is ' num2str(Specificity) ' !']);
-save([ResultantFolder filesep 'Specificity.mat'], 'Specificity');
 
-% Calculating w
-W_Calculate_SVM(Subjects_Data, Subjects_Label, Pre_Method, ResultantFolder);
+if nargin > 4
+  save([ResultantFolder filesep 'Y.mat'], 'Y_group1', 'Y_group0');
+  save([ResultantFolder filesep 'Category.mat'], 'Category_group1', 'Category_group0');
+  save([ResultantFolder filesep 'WrongInfo.mat'], 'group0_Wrong_Quantity', 'group0_Wrong_ID', 'group1_Wrong_Quantity', 'group1_Wrong_ID');
+  save([ResultantFolder filesep 'Accuracy.mat'], 'Accuracy');
+  save([ResultantFolder filesep 'Sensitivity.mat'], 'Sensitivity');
+  save([ResultantFolder filesep 'Specificity.mat'], 'Specificity');
+  % Calculating w
+  W_Calculate_SVM(Subjects_Data, Subjects_Label, Pre_Method, ResultantFolder);
+end
+
